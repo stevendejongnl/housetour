fc8ba55e27e7f2537d8be63e78ce0d5706e81113
Steven de Jong <sdejong@cloudsuite.com>
2025-07-03 13:11:48 +0000

    Refactor area loading in index route and template to use dynamic area data

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from area.routes import area_blueprint
+from area.routes import area_blueprint, get_available_areas, get_area_title
 
 __version__ = '0.1.0'
 app = Flask(__name__)
@@ -9,14 +9,16 @@ app.register_blueprint(area_blueprint)
 
 @app.route('/')
 def index():
-    area_urls = [
-        rule.rule
-        for rule in app.url_map.iter_rules()
-        if rule.endpoint.startswith(f'{area_blueprint.name}.')
+    area_list = [
+        {
+            'url': f'/area/{area}',
+            'name': get_area_title(area)
+        }
+        for area in get_available_areas()
     ]
     return render_template(
         'index.html',
         title='House Tour',
         description='Explore the different areas of the house.',
-        area_urls=area_urls
+        areas=area_list
     )
